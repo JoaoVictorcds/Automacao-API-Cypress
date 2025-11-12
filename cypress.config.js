401558d3490f1ed82b45717640e5316c6934530d
Joao victor Cruz <joaovitorcruzsouza@gmail.com>
2025-11-11 23:44:28 -0300

    atualizando ignore

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,9 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "ran7jz",
   e2e: {
+    
     defaultCommandTimeout:6000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
